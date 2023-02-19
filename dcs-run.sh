4daa116b61b624aa1b04259fb81e9b73cf5b600d
Mark Dickie <mark.dickie@gmail.com>
2023-02-19 22:27:21 +0100

    Use Esync when launching DCS

--- dcs-run.sh
+++ dcs-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WORKDIR=notset
 
-WINEPREFIX=~/Games wine C:\\Program\ Files\\Eagle\ Dynamics\\DCS\ World\ OpenBeta\ Server\\bin\\DCS.exe --server --norender --quiet -w $WORKDIR
+WINEESYNC=1 WINEPREFIX=~/Games wine C:\\Program\ Files\\Eagle\ Dynamics\\DCS\ World\ OpenBeta\ Server\\bin\\DCS.exe --server --norender --quiet -w $WORKDIR
