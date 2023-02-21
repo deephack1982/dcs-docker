265e627a02995d313b79556ac2b2ff877493e783
Mark Dickie <mark.dickie@gmail.com>
2023-02-21 23:30:17 +0100

    Added wbemprox override

--- dcs-run.sh
+++ dcs-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WORKDIR=notset
 
-WINEESYNC=1 WINEPREFIX=~/Games wine C:\\Program\ Files\\Eagle\ Dynamics\\DCS\ World\ OpenBeta\ Server\\bin\\DCS.exe --server --norender --quiet -w $WORKDIR
+WINEDLLOVERRIDES=wbemprox=n WINEESYNC=1 WINEPREFIX=~/Games wine C:\\Program\ Files\\Eagle\ Dynamics\\DCS\ World\ OpenBeta\ Server\\bin\\DCS.exe --server --norender --quiet -w $WORKDIR
